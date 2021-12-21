9259d0aa8118a75995da2a6c54fd76cc99fcd5ff
David Power <dpower19@gmail.com>
2021-12-22 08:35:28 +1100

    Initial Commit

---
+++ Plant Tag Organiser/Plant_Tag_OrganiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_Tag_OrganiserApp.swift
+//  Plant Tag Organiser
+//
+//  Created by David Power on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Plant_Tag_OrganiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
